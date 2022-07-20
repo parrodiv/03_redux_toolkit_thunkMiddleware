fbb769bd607422797d3bf9c6fc698899e6a933d9
Alessandro Parrilla <paggio96@gmail.com>
2022-07-20 07:25:40 +0200

    Building PostsExerpt component

---
+++ src/features/posts/PostsExcerpt.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionsButtons from './ReactionsButtons'
+
+const PostsExcerpt = ( { post }) => {
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <p className='postCredit'>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionsButtons post={post} />
+    </article>
+  )
+}
+
+export default PostsExcerpt
